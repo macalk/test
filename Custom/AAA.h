d1c0c229b5ff816381f83f64808090241d3022cf
鲁振 <macalk@163.com>
2018-11-21 15:52:18 +0800

    1111

---
+++ Custom/AAA.h
@@ -0,0 +1,17 @@
+//
+//  AAA.h
+//  Custom
+//
+//  Created by 鲁振 on 2018/11/21.
+//  Copyright © 2018 鲁振. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AAA : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Custom/ViewController.m
+++ Custom/ViewController.m
@@ -18,9 +18,4 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-- (void)customAD {
-    
-}
-
-
 @end
